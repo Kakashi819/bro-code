7d34754fb16551a923f774e7225aff36ff6cc2ba
Ayush Singh <ayushsinghkushwah8@gmail.com>
2023-07-26 18:27:20 +0530

    updated validation

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,71 +1,63 @@
-import { useState } from "react";
 import React from "react";
 import data from "../Data/data.js";
-import InputForm from "../components/InputForm";
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import imgsrc from "../images/banner.png";
+import "./Signup.css";
 
 const Signup = () => {
-	const [fields, setFields] = useState({
-		name: "",
-		email: "",
-		password: "",
-		confirm_password: "",
-	});
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log("handle submitted");
-		console.log(fields, "initalValue");
-	};
-
+	// error validation
 	const SignupSchema = Yup.object().shape({
-		name: Yup.string()
-			.min(2, "Too Short!")
-			.max(70, "Too Long!")
-			.required("Required"),
-		email: Yup.string().email("Invalid email").required("Required"),
+		name: Yup.string().required("Name is required"),
+		email: Yup.string().email("Invalid email").required("Email is required"),
 		password: Yup.string()
-			.min(2, "Too Short!")
-			.max(8, "Too Long!")
-			.required("Required"),
-
+			.min(8, "Password must be at least 8 characters")
+			.required("Password is required"),
 		confirm_password: Yup.string()
-			.min(2, "Too Short!")
-			.max(8, "Too Long!")
-			.required("Required"),
+			.oneOf([Yup.ref("password"), null], "Passwords must match")
+			.required("Confirm Password is required"),
 	});
+
 	return (
 		<div className="main-form-container">
-			<div className="signup-image">
-				<img src="banner.png" alt="banner image" />
-			</div>
-			<div className="main-form">
-				<h2>Welcome to Codeforces</h2>
-				<p>this is exiciting place for all the competitive programmers.</p>
-				<Formik
-					initialValues={fields}
-					onSubmit={handleSubmit}
-					validationSchema={SignupSchema}
-				>
-					{(props) => (
-						<form onSubmit={handleSubmit}>
-							{data.map((input, index) => (
-								<React.Fragment key={index}>
-									<InputForm
-										key={input.key}
-										fields={fields}
-										setFields={setFields}
-										{...input}
-									/>
-									{props.errors[input.name] && props.touched[input.name] && (
-										<div className="error">{props.errors[input.name]}</div>
-									)}
-								</React.Fragment>
-							))}
-							<button type="submit">Submit</button>
-						</form>
-					)}
-				</Formik>
+			<div className="form-container">
+				<div className="signup-image">
+					<img src={imgsrc} alt="banner image" className="singup-image-main" />
+				</div>
+				<div className="main-form">
+					<h2>Welcome to Codeforces</h2>
+					<p>this is an exciting place for all the competitive programmers.</p>
+					<Formik
+						initialValues={{
+							name: "",
+							email: "",
+							password: "",
+							confirm_password: "",
+						}}
+						onSubmit={(values) => {
+							console.log("handle submitted");
+							console.log(values, "form data");
+							alert("form is validated");
+						}}
+						validationSchema={SignupSchema}
+					>
+						{(errors, touched) => (
+							<Form>
+								{data.map((input, index) => (
+									<div key={index}>
+										<label>{input.name}</label>
+										<Field {...input} />
+										{errors.name && touched.name ? (
+											<div>{errors.name}</div>
+										) : null}
+										<ErrorMessage {...input} />
+									</div>
+								))}
+								<button type="submit">Submit</button>
+							</Form>
+						)}
+					</Formik>
+				</div>
 			</div>
 		</div>
 	);
